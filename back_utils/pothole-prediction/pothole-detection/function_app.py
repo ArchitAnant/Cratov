fc26472e0daf61eb9bfd612d0b7800ccdc468624
ArchitAnant <architanant5@gmail.com>
2025-06-29 13:22:07 +0530

    changing processing to blob storage

--- back_utils/pothole-prediction/pothole-detection/function_app.py
+++ back_utils/pothole-prediction/pothole-detection/function_app.py
@@ -1,37 +1,44 @@
 import azure.functions as func
 import json
 import logging
-import io
 from HttpTrigger.inference import ModelWrapper
-import base64
+from utils import get_image_from_blob,fetch_images
 
 app = func.FunctionApp()
+model = ModelWrapper()
 
 @app.route(route="predictPothole", auth_level=func.AuthLevel.FUNCTION)
 def predictPothole(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Trigger function triggered to predict potholes.')
     try:
-        request_body = req.get_json()
+        post_id = req.params.get('postid')
 
-        if not request_body or 'image' not in request_body:
+        if not post_id:
             return func.HttpResponse(
-                json.dumps({"error": "Invalid request. 'image' field is required."}),
+                json.dumps({"error": "Invalid request. 'postid' field is required."}),
                 status_code=400,
                 mimetype="application/json",
                 headers={"Access-Control-Allow-Origin": "*"}
             )
         
-        base64_image = request_body['image']
-        image_bytes = base64.b64decode(base64_image)
-        image = io.BytesIO(image_bytes)
-        model = ModelWrapper()
-        result = model.predict(image)
-        data = {
-            "pdt_tag": result["pdt_tag"],
-            "pdt_prob": result["pdt_prob"]
-        }
+        image_list = fetch_images(post_id)
+        image_dict = get_image_from_blob(image_list)
+
+        image_keys = list(image_dict.keys())
+        image_datas = list(image_dict.values())
+
+        img_dict = {}
+
+        for i in range(len(image_keys)):            
+            result = model.predict(image_datas[i])
+            data = {
+                "pdt_tag": result["pdt_tag"],
+                "pdt_prob": result["pdt_prob"]
+            }
+            img_dict[image_keys[i]] = data
+
         return func.HttpResponse(
-            json.dumps(data),
+            json.dumps(img_dict),
             status_code=200,
             mimetype="application/json",
             headers={"Access-Control-Allow-Origin": "*"}
