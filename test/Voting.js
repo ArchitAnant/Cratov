03091693e834ed6e846a6aa8001c758f02e44bf8
ArchitAnant <architanant5@gmail.com>
2025-08-10 20:19:11 +0530

    Fix: Passing : 4 | Failing : 1 :: #11

--- test/Voting.js
+++ test/Voting.js
@@ -2,46 +2,56 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Voting Contract", function () {
-  let Voting, voting, owner;
+  let Voting, voting;
 
   beforeEach(async () => {
-    [owner, user1, user2] = await ethers.getSigners();
     Voting = await ethers.getContractFactory("Voting");
-    voting = await Voting.deploy();
-    await voting.deployed();
+    voting = await Voting.deploy(); // No need for .deployed()
   });
 
-  it("should allow a user to vote and retrieve voters", async () => {
-    await voting.voteOrAddPost("post1", "user1");
+  it("should allow a user to upvote a post", async () => {
+    await expect(voting.voteOrAddPost("post1", "user1"))
+      .to.emit(voting, "PostUpvoted")
+      .withArgs("post1", "user1");
+
     const voters = await voting.getVoters("post1");
-    expect(voters).to.include("user1");
+    expect(voters).to.deep.equal(["user1"]);
   });
 
-  it("should not allow the same user to vote twice", async () => {
+  it("should not allow the same user to upvote twice", async () => {
     await voting.voteOrAddPost("post1", "user1");
-    await expect(voting.voteOrAddPost("post1", "user1")).to.be.revertedWith("User has already voted");
+    await expect(voting.voteOrAddPost("post1", "user1"))
+      .to.be.revertedWith("User has already voted");
+  });
+
+  it("should allow another user to upvote same post", async () => {
+    await voting.voteOrAddPost("post1", "user1");
+    await voting.voteOrAddPost("post1", "user2");
+
+    const voters = await voting.getVoters("post1");
+    expect(voters).to.have.members(["user1", "user2"]);
   });
 
   it("should allow a user to unvote", async () => {
     await voting.voteOrAddPost("post1", "user1");
-    await voting.unvoteUser("post1", "user1");
-    const voters = await voting.getVoters("post1");
-    expect(voters).to.not.include("user1");
-  });
-
-  it("should return an empty array for non-existent post", async () => {
-    const voters = await voting.getVoters("no_such_post");
-    expect(voters.length).to.equal(0);
-  });
-
-  it("should remove a post and all associated votes", async () => {
-    await voting.voteOrAddPost("post1", "user1");
     await voting.voteOrAddPost("post1", "user2");
-    await voting.removePost("post1");
+
+    await expect(voting.unvoteUser("post1", "user1"))
+      .to.emit(voting, "PostDownvoted")
+      .withArgs("post1", "user1");
 
     const voters = await voting.getVoters("post1");
-    expect(voters.length).to.equal(0);
+    expect(voters).to.deep.equal(["user2"]);
+  });
 
-    await expect(voting.voteOrAddPost("post1", "user1")).to.not.be.reverted;
+  it("should allow removing a post", async () => {
+    await voting.voteOrAddPost("post1", "user1");
+
+    await expect(voting.removePost("post1"))
+      .to.emit(voting, "RemovePost")
+      .withArgs("post1");
+
+    const voters = await voting.getVoters("post1");
+    expect(voters).to.deep.equal([]);
   });
 });
